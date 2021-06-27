4f3ae9631a729179a00669c65edafd618ae1e56d
amstel1 <spareemail@yandex.ru>
2021-06-27 12:25:02 +0300

    Update ab_sample_size.py

--- ab_sample_size.py
+++ ab_sample_size.py
@@ -23,3 +23,32 @@ def calc_sample_size(alpha, power, p, pct_mde):
     return (t_alpha2 * sd1 + t_beta * sd2) * (t_alpha2 * sd1 + t_beta * sd2) / (delta * delta)
 
 print(calc_sample_size(alpha=.05, power=.80, p=.01, pct_mde=0.5))
+
+#####################
+#conversion rates
+def get_confidence_ab_test(click_a, num_a, click_b, num_b):
+    rate_a = click_a / num_a
+    rate_b = click_b / num_b
+    std_a = np.sqrt(rate_a * (1 - rate_a) / num_a)
+    std_b = np.sqrt(rate_b * (1 - rate_b) / num_b)
+    z_score = (rate_b - rate_a) / np.sqrt(std_a**2 + std_b**2)
+    return norm.cdf(z_score)
+
+print(get_confidence_ab_test(click_a, num_a, click_b, num_b))
+
+# Draw 10000 samples of possible rates for a and b
+n = 10000
+rates_a = norm(rate_a, std_a).rvs(n)
+rates_b = norm(rate_b, std_b).rvs(n)
+b_better = (rates_b > rates_a).mean()
+print(f"B is better than A {b_better:0.1%} of the time")
+
+a_dist = np.zeros(num_a)
+a_dist[:click_a] = 1
+b_dist = np.zeros(num_b)
+b_dist[:click_b] = 1
+zscore, prob = ttest_ind(a_dist, b_dist, equal_var=False)
+print(f"Zscore is {zscore:0.2f}, p-value is {prob:0.3f} (two tailed), {prob/2:0.3f} (one tailed)")
+
+stat, p_value = mannwhitneyu(a_dist, b_dist, alternative="less")
+print(f"Mann-Whitney U test for null hypothesis B <= A is {p_value:0.3f}")
